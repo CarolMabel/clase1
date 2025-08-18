f1b6c043999306ca8dd0e96db844e1556dd9e767
Carol Alvarez <cmaaalvareza@gmail.com>
2025-08-18 15:43:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
   background(220,162,252);
 }
-
 function draw() {
  d= random(10,60);
   colorMode(HSB,360,100,100);
